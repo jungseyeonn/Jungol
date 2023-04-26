d9baf7947c06dc7b7aba84f6321d751a0bf528d8
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-26 17:39:43 +0900

    504번 문제

---
+++ Jungol/src/q504/Main.java
@@ -0,0 +1,12 @@
+package q504;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("(@) (@)");
+		System.out.println("(=^.^=)");
+		System.out.println("(-m-m-)");
+	}
+
+}
