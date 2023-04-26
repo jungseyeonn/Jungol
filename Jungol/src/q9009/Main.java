6f720d1ca7620ae4cf62ed2f76e776f14aac95d6
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-26 17:28:35 +0900

    9009번문제

---
+++ Jungol/src/q9009/Main.java
@@ -0,0 +1,11 @@
+package q9009;
+
+public class Main {
+	public static void main(String[] args) {
+		int a=10;
+		char b='A';
+		
+		System.out.printf("a = %d\n",a);
+		System.out.printf("b = %c",b);
+	}
+}
