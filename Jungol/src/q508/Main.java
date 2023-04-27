5665f13d72d467afaf1e01ac71b02f21503e8250
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-27 10:20:18 +0900

    508

---
+++ Jungol/src/q508/Main.java
@@ -0,0 +1,15 @@
+package q508;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.printf("%10s%10s%10s\n","item","count","price");
+		System.out.printf("%10s%10d%10d\n","pen",20,100);
+		System.out.printf("%10s%10d%10d\n","note",5,95);
+		System.out.printf("%10s%10d%10d","eraser",110,97);
+
+//		System.out.println("      item     count     price");
+//		System.out.println("       pen        20       100");
+//		System.out.println("      note         5        95");
+//		System.out.println("    eraser       110        97");
+	}
+}
