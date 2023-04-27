166ea8a6dfd3b8a476e9883c214564813867629a
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-27 17:48:16 +0900

    107

---
+++ Jungol/src/q107/Main.java
@@ -0,0 +1,9 @@
+package q107;
+
+public class Main {
+	public static void main(String[] args) {
+		double d1 = 80.5, d2 = 22.34;
+		
+		System.out.printf("%10.2f%10.2f%10.2f", d1,d2,(d1+d2));
+	}
+}
