a82c11a5ed5634fcf786d6dd00c20e0fb3f7c0aa
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-27 12:14:12 +0900

    9006

---
+++ Jungol/src/q9006/Main.java
@@ -0,0 +1,11 @@
+package q9006;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("수를 출력할 때는 따옴표를 생략합니다.\n123\n6.500000");
+		
+		double d1 = 6.5;
+		System.out.println(d1);
+		System.out.printf("%.3f\n", d1);
+	}
+}
