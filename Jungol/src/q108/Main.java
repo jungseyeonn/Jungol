4861cee587a69ec488ebf39e312597309074fcc7
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-27 18:08:38 +0900

    108

---
+++ Jungol/src/q108/Main.java
@@ -0,0 +1,10 @@
+package q108;
+
+public class Main {
+	public static void main(String[] args) {
+		int num1 = 50;
+		double num2 = 100.12;
+		
+		System.out.printf("%.2f * %d = %.0f",num2,num1,num1*num2);
+	}
+}
