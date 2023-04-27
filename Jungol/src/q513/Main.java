2895f8cce091ea1d6a4d3a0f7257e902fe7dcae9
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-27 17:01:23 +0900

    513

---
+++ Jungol/src/q513/Main.java
@@ -0,0 +1,12 @@
+package q513;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double yd = 91.44;
+	    double in = 2.54;
+		
+		System.out.printf("%4.1fyd = %5.1fcm\n", 2.1, yd*2.1);
+		System.out.printf("%4.1fin = %5.1fcm\n", 10.5, in*10.5);
+	}
+}
