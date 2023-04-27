0287e50e7a720e6519c89fb0a8055557ae1c71b5
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-27 17:07:12 +0900

    9013

---
+++ Jungol/src/q9013/Main.java
@@ -0,0 +1,17 @@
+package q9013;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = 1.2340;
+		double y = 10.3459;
+		
+		System.out.println("전체 7자리로 맞추고 소수 4자리까지 출력");
+		System.out.printf("x = %7.4f\n", x);
+		System.out.printf("y = %7.4f\n\n", y);
+		System.out.println("소수 2자리까지 출력(반올림)");
+		System.out.printf("x = %.2f\n", x);
+		System.out.printf("y = %.2f\n", y);
+	}
+}
