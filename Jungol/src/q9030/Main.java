8e579a76285d3be7dcb21b33eeffedfaf6438fac
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-28 13:58:28 +0900

    9030

---
+++ Jungol/src/q9030/Main.java
@@ -0,0 +1,22 @@
+package q9030;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		sc.close();
+		
+		if(num>10) {
+			System.out.println(num);
+			if(num>10) {
+				System.out.println("10보다 큰 수를 입력하셨습니다.");
+			}
+		} else {
+			System.out.println(num);
+		}
+	}
+}
