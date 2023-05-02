421c8f02753cd91a95ca92a293332f9866f34ca6
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-02 11:52:32 +0900

    9011

---
+++ Jungol/src/q9011/Main.java
@@ -0,0 +1,17 @@
+package q9011;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		int sum = 0;
+		sum = num1 + num2;
+		System.out.println(num1 + " + " + num2 + " = " + sum);
+		num1 = 30;
+		num2 = 40;
+		sum = num1 + num2;
+		System.out.println(num1 + " + " + num2 + " = " + sum);	
+	}
+}
