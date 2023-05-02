7f358a9ebc9721f9807a0a198de8575d0e67c573
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-02 14:41:18 +0900

    9066

---
+++ Jungol/src/q9066/Main.java
@@ -0,0 +1,10 @@
+package q9066;
+
+public class Main {
+	public static void main(String[] args) {
+		char [] c = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+		for(int i=25;i>=0;i--) {
+			System.out.print(c[i]+" ");
+		}
+	}
+}
