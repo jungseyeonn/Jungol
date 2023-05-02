139eb0782b58d956f598e7697870b4f49d4807c4
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-02 15:11:55 +0900

    556

---
+++ Jungol/src/q556/Main.java
@@ -0,0 +1,13 @@
+package q556;
+
+public class Main {
+	public static void main(String[] args) {
+		int ar [] = {1,2,3,4,5,6,7,8,9,10};
+		for (int i=0;i<ar.length;i++) {
+			System.out.print(ar[i]);
+			if (i<ar.length-1) {
+				System.out.print(" ");
+			}
+		}
+	}
+}
