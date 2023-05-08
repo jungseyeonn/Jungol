493a1e35bffc1c88467af7a6e4e4b58a21011838
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-08 09:39:58 +0900

    532

---
+++ Jungol/src/q532/Main.java
@@ -0,0 +1,20 @@
+package q532;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double num1 = sc.nextDouble();
+		double num2 = sc.nextDouble();
+		sc.close();
+		
+		if (num1>=4.0 && num2>=4.0) {
+			System.out.print("A");
+		} else if (num1>=3.0 && num2>=3.0) {
+			System.out.print("B");
+		} else {
+			System.out.print("C");
+		}
+	}
+}
