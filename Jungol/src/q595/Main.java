5661e60f90aaf02b9ae0610266be9ddc16970243
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-10 15:56:31 +0900

    595

--- Jungol/src/q595/Main.java
+++ Jungol/src/q595/Main.java
@@ -3,6 +3,7 @@ package q595;
 public class Main {
 	public static void main(String[] args) {
 		String str = new String("Hong Gil Dong");
+		//1.
 		for (int i=3;i<7;i++) {
 			System.out.print(str.charAt(i));
 		}
@@ -10,5 +11,8 @@ public class Main {
 //		System.out.print(str.charAt(4));
 //		System.out.print(str.charAt(5));
 //		System.out.print(str.charAt(6));
+		
+		//2.
+		System.out.println(str.substring(3,7));
 	}
 }
