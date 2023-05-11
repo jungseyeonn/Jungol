d60e011d9bb06aa1ceed3d97f2b968f1a4caf618
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-11 11:02:31 +0900

    558 답추가

--- Jungol/src/q558/Main.java
+++ Jungol/src/q558/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] ar = new int[100];
+		
 		for (int i = 0; i < ar.length; i++) {
 			ar[i] = sc.nextInt();
 			if (ar[i] == 0) {
@@ -13,6 +14,17 @@ public class Main {
 					System.out.print(ar[j] + " ");
 				} break;
 			}
+		}
+		
+		//선생님 답안
+		int inp = 0, idx = 0;
+		do {
+			inp = sc.nextInt();
+			ar[idx++] = inp;
+		} while (inp != 0);
+
+		for (int i = idx - 2; i >= 0; i--) {
+			System.out.print(ar[i] + " ");
 		}
 		sc.close();
 	}
