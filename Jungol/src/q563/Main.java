fc61b34060606cb5ac57d9a96feee6c94347658a
Class01 <Class01@DESKTOP-AHAB51C>
2023-05-30 19:51:50 +0900

    563

---
+++ Jungol/src/q563/Main.java
@@ -0,0 +1,22 @@
+package q563;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [] num = new int [10];
+
+		for(int i=0;i<num.length;i++) {
+			num[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		Arrays.sort(num);
+		
+		for (int i=num.length-1;i>=0;i--) {
+			System.out.print(num[i] + " ");
+		}
+	}
+}
